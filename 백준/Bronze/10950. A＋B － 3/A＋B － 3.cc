60ecf76dda024261b5e27114be581fb551fd172e
서희연 <154355151+shyun03@users.noreply.github.com>
2024-06-04 11:00:37 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+
+	int a, b;
+	for (int i = 0; i < n; ++i)
+	{
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+	
+	return 0;
+}

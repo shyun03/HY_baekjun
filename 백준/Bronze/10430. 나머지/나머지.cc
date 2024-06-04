e82753bf157331b858d3dbd30174e247ad428611
서희연 <154355151+shyun03@users.noreply.github.com>
2024-06-04 11:04:54 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int main(void)
+{
+	int A;
+	int B;
+	int C;
+
+	scanf("%d %d %d", &A, &B, &C);
+
+	printf("%d\n", (A + B) % C);
+	printf("%d\n", (((A % C) + (B % C)) % C));
+	printf("%d\n", (A*B) % C);
+	printf("%d\n", ((A % C)*(B % C)) % C);
+
+	return 0;
+}

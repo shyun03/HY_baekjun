e9098d7d8598e554f72abba9c62070cd3e24db0f
서희연 <154355151+shyun03@users.noreply.github.com>
2024-06-04 11:11:38 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+    int result;
+    
+    scanf("%d\n", &x);
+    scanf("%d", &y);
+    
+    result = x + y;
+    
+    printf("%d", result);
+
+    return 0;
+}

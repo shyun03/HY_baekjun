8e842cc9ae8f5b17e267828d2e8d0e5f62016a9b
서희연 <154355151+shyun03@users.noreply.github.com>
2024-06-04 11:12:32 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void) 
+{
+	int n;
+	scanf("%d", &n);
+	for (int k = 1; k <= n; k++) 
+	{
+		for (int i = 1; i <= n - k; i++) 
+		{
+			printf(" ");
+		}
+		for (int j = 1; j <= k; j++) 
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}

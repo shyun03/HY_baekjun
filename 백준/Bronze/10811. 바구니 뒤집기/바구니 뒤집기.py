5e29b178fe344c1de2e8eb86c6e5555b472dcb2d
서희연 <154355151+shyun03@users.noreply.github.com>
2025-04-17 10:12:01 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,4 +1,4 @@
-#바구니 뒤집기
+#바구니 뒤집기 코드 줄이기
 
 n, m = map(int, input().split())
 num = []
@@ -8,15 +8,7 @@ for x in range(n):
 
 for i in range(m):
     a, b = map(int, input().split())
-    mat = num[a-1:b]
+    num[a-1:b] = reversed(num[a-1:b])
     
-    mat.reverse()
 
-    cnt = 0
-    while(cnt<len(mat)):
-        for j in range(a-1, b):
-            num[j] = mat[cnt]
-            cnt += 1
 print(*num)
-
-      
